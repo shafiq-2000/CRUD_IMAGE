1efd6c46c5804843faca62837188a4f715124cfa
Shafiqul Islam <shafiq53471@gmail.com>
2024-04-20 23:12:44 +0600

    initial commit

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "", "test");
+// if ($connect) {
+//     echo "<script>alert('database success') </script>";
+// } else {
+//     echo "<script>alert('database failed') </script>";
+// }
+
+
+?>
